3ec80245f4ff824e36a93ac0290b3be3062daa99
Anna-Mariya Mateyna <anna-mariya.mateyna@gmx.de>
2018-03-26 23:46:12 +0200

    Shellscript zum Ausführen

---
+++ analysis_reports/main.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Shellskript"
+
+python3 main.py
+pdflatex main.tex
+
+rm base64decoder.tex
+rm cpuarchitecture.tex
+rm cryptomaterial.tex
+rm exploitmitigations.tex
+rm filehashes.tex
+rm filetype.tex
+rm initsystems.tex
+rm ipandurifinder.tex
+rm main.log
+rm main.aux
+rm malwarescanner.tex
+rm meta.tex
+rm softwarecomponents.tex
+rm stringevaluator.tex
+rm printablestrings.tex
+rm unpacker.tex
+rm usersandpasswords.tex
