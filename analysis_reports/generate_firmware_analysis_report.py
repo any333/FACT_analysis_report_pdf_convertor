1a524560e44fff7baf08ad3a1dc1697a51b1e6f1
Anna-Mariya Mateyna <anna-mariya.mateyna@gmx.de>
2018-04-10 21:53:37 +0200

    parsing arguments with argparse

--- analysis_reports/generate_firmware_analysis_report.py
+++ analysis_reports/generate_firmware_analysis_report.py
@@ -3,13 +3,10 @@ import requests
 import jinja2
 import os
 import sys
+import argparse
 from filter import byte_number_filter, nice_unix_time, nice_number_filter, filter_latex_special_chars, \
     count_elements_in_list, convert_base64_to_png_filter, check_if_list_empty, split_hash
 
-HOST = "http://localhost:5000"
-PATH = "/rest/firmware/"
-FIRMWARE_UID = sys.argv[1]
-GET_URL = HOST + PATH + FIRMWARE_UID
 
 latex_jinja_env = jinja2.Environment(
     block_start_string='\BLOCK{',
@@ -45,24 +42,22 @@ def _make_get_requests(url):
     return response_dict
 
 
-firmware_data = _make_get_requests(GET_URL)
-meta_data = firmware_data['firmware']['meta_data']
-analysis = firmware_data['firmware']['analysis']
-
-
-def create_main_tex():
+def create_main_tex(meta_data):
     template = latex_jinja_env.get_template('templates/main_template.tex')
 
     main_tex_file = template.render(meta_data=meta_data)
+    main_tex_file_name = meta_data['device_name']+"_Analysis_Report.tex"
+    main_tex_file_name = main_tex_file_name.replace(" ", "_")
+    main_tex_file_name = main_tex_file_name.replace("/", "__")
 
-    fh = open("firmware_analysis_report.tex", 'w')
+    fh = open(main_tex_file_name, 'w')
     fh.write(main_tex_file)
     fh.close()
 
     pass
 
 
-def create_meta_tex():
+def create_meta_tex(meta_data):
     template = latex_jinja_env.get_template('templates/meta_data_template.tex')
 
     meta_tex_file = template.render(meta_data=meta_data)
@@ -74,13 +69,13 @@ def create_meta_tex():
     pass
 
 
-def create_analysis_texs():
+def create_analysis_texs(analysis):
     for processed_analysis in analysis:
 
         selected_analysis = analysis[processed_analysis]
 
         if os.path.isfile('templates/' + processed_analysis + '_template.tex'):
-            #del selected_analysis['summary']
+            del selected_analysis['summary']
 
             template = latex_jinja_env.get_template('templates/' + processed_analysis + '_template.tex')
 
@@ -92,6 +87,26 @@ def create_analysis_texs():
             fh.close()
 
             pass
+
+
+def create_analysis_texs_with_summary(analysis):
+    for processed_analysis in analysis:
+
+        selected_analysis = analysis[processed_analysis]
+
+        if os.path.isfile('templates/' + processed_analysis + '_template.tex'):
+
+            template = latex_jinja_env.get_template('templates/' + processed_analysis + '_template.tex')
+
+            analysis_tex_file = template.render(selected_analysis=selected_analysis)
+
+            processed_texfile = processed_analysis + ".tex"
+            fh = open(processed_texfile, 'w')
+            fh.write(analysis_tex_file)
+            fh.close()
+
+            pass
+
 
 def delete_generated_files():
     dir = "./"
@@ -108,9 +123,56 @@ def delete_generated_files():
     os.remove(os.path.join(dir, "entropy_analysis_graph.png"))
 
 
-setup_jinja_filters()
-create_main_tex()
-create_meta_tex()
-create_analysis_texs()
-#delete_generated_files()
-print("all .tex files successfully created")
+def create_pdf_report(meta_data):
+    main_tex_file_name = meta_data['device_name'] + "_Analysis_Report.tex"
+    main_tex_file_name = main_tex_file_name.replace(" ", "_")
+    main_tex_file_name = main_tex_file_name.replace("/", "__")
+    os.system("env buf_size=1000000 pdflatex " + main_tex_file_name)
+
+
+def main():
+    HOST = "http://localhost:5000"
+    PATH = "/rest/firmware/"
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", dest="summary", action="store_false")
+    parser.add_argument("-uid", dest="uid")
+
+    args = parser.parse_args()
+
+    if args.summary:
+
+        FIRMWARE_UID = args.uid
+        GET_URL = HOST + PATH + FIRMWARE_UID
+
+        firmware_data = _make_get_requests(GET_URL)
+        meta_data = firmware_data['firmware']['meta_data']
+        analysis = firmware_data['firmware']['analysis']
+
+        setup_jinja_filters()
+        create_main_tex(meta_data)
+        create_meta_tex(meta_data)
+        create_analysis_texs(analysis)
+        create_pdf_report(meta_data)
+        delete_generated_files()
+        print("Analysis report generated successfully.")
+    else:
+
+        FIRMWARE_UID = args.uid
+        GET_URL = HOST + PATH + FIRMWARE_UID
+
+        firmware_data = _make_get_requests(GET_URL)
+        meta_data = firmware_data['firmware']['meta_data']
+        analysis = firmware_data['firmware']['analysis']
+
+        setup_jinja_filters()
+        create_main_tex(meta_data)
+        create_meta_tex(meta_data)
+        create_analysis_texs_with_summary(analysis)
+        create_pdf_report(meta_data)
+        delete_generated_files()
+        print("Analysis report generated successfully.")
+
+
+if __name__ == "__main__":
+    main()
